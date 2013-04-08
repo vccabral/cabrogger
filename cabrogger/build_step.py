f6f99b52d5a98ed11841dc3e251a40d13e5d043e
Victor Cabral <vccabral@gmail.com>
2013-04-07 20:25:30 -0400

    db

---
+++ cabrogger/build_step.py
@@ -0,0 +1,11 @@
+import subprocess, signal
+
+p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+
+out, err = p.communicate()
+
+for line in out.splitlines():
+	if 'python' in line:
+		pid = int(line.split(None, 1)[0])
+		os.kill(pid, signal.SIGKILL)
+
